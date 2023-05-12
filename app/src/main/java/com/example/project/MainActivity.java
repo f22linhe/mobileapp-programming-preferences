cfbb9890c738b5c9e76a9d4fbebd0c4bb2813e53
Lina H <f22linhe@student.his.se>
2023-05-12 14:56:30 +0200

    Lade till en ny activity (SecondActivity) med tillhörande layout. Lade till TextView och knapp i activity_main.xml. Lade till EditText och knapp i activity_second.xml. Gjorde det möjligt att navigera mellan aktiviteterna.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button ToSecondActivity = findViewById(R.id.my_button1);
+        ToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
